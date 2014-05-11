85afbdb7e22d3aae1595824cfe3e87f52fa17e82
Max Bernstein <tekknolagi@gmail.com>
2014-05-10 17:41:14 -0700

    new url

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require './db'
 
 class PalyIO < Sinatra::Base
   configure do
-    @@host = "http://url.palyvoice.com"
+    @@host = "http://paly.io"
 
     def gen_rand size=6
       charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
