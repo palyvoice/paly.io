2d65b16e722634f91e2fa0cb051e1fed435cf5fe
Max Bernstein <tekknolagi@gmail.com>
2014-05-11 21:13:06 -0700

    new env variable

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require './db'
 
 class PalyIO < Sinatra::Base
   configure do
-    @@host = "http://paly.io"
+    @@host = ENV['PALYIO_HOSTNAME']
 
     def gen_rand size=6
       charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
