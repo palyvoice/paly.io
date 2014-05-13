1ac4e817fa1750471175890f4c2fc93a3c8da5e4
Max Bernstein <tekknolagi@gmail.com>
2014-05-12 20:59:59 -0700

    fixed up restart

--- scripts/mac_restart.sh
+++ scripts/mac_restart.sh
@@ -1,2 +1,2 @@
-$PALYIO_HOME_DIR$PALYIO_WORKING_DIR'scripts/mac_stop.sh'
-$PALYIO_HOME_DIR$PALYIO_WORKING_DIR'scripts/mac_start.sh'
+cat $PALYIO_HOME_DIR$PALYIO_WORKING_DIR'tmp/pids/unicorn.pid' | xargs kill -QUIT
+unicorn -c $PALYIO_HOME_DIR$PALYIO_WORKING_DIR'unicorn.rb' -E development -D
--- scripts/mac_stop.sh
+++ scripts/mac_stop.sh
@@ -1,3 +1,3 @@
-cat $PALYIO_HOME_DIR$PALYIO_WORKING_DIR'tmp/pids/*' | xargs kill -QUIT
+cat $PALYIO_HOME_DIR$PALYIO_WORKING_DIR'tmp/pids/unicorn.pid' | xargs kill -QUIT
 nginx -s stop
 mysql.server stop
