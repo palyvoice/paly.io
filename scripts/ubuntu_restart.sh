1b9698a284ee3b42cffe939913c8f68992180eb6
Christopher Hinstorff <chris@chinstorff.com>
2014-05-13 12:48:09 -0700

    update ubuntu_restart, add README

--- scripts/ubuntu_restart.sh
+++ scripts/ubuntu_restart.sh
@@ -1,3 +1,11 @@
-$PALYIO_HOME_DIR$PALYIO_WORKING_DIR'scripts/ubuntu_stop.sh'
-$PALYIO_HOME_DIR$PALYIO_WORKING_DIR'scripts/ubuntu_start.sh'
+cd $PALYIO_HOME_DIR$PALYIO_WORKING_DIR
+
+cat tmp/pids/unicorn.pid | xargs kill -QUIT
+rm tmp/pids/unicorn.pid
+rm tmp/sockets/unicorn.sock
+
+bundle install
+unicorn -c unicorn.rb -E development -D
+
+cd -
 
