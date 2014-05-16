69cee2f819731b0ad28a7da8ff84074601487a31
Max Bernstein <tekknolagi@gmail.com>
2014-05-16 13:20:37 -0700

    fixed api tests

--- spec/api_test.rb
+++ spec/api_test.rb
@@ -24,7 +24,7 @@ describe PalyIO::API do
   context :api do
     describe 'GET /shorten' do
       it 'shortens a given url' do
-        get '/shorten', { :url => Faker::Internet.http_url, :custom => "" }
+        get '/shorten', { :url => Faker::Internet.http_url, :custom => '' }
 
         hash = JSON.parse(last_response.body)
         hash['success'].should == true
@@ -34,7 +34,7 @@ describe PalyIO::API do
     describe 'GET /whatis' do
       it 'gets the url for a given key' do
         url = Faker::Internet.http_url
-        key = Faker.numerify("######")
+        key = Faker.numerify('######')
         link = link_hash gen_link(key, url)
 
         get '/whatis', { :key => key }
@@ -48,7 +48,7 @@ describe PalyIO::API do
     describe 'GET /meta' do
       it 'gets the meta information for a given key' do
         url = Faker::Internet.http_url
-        key = Faker.numerify("######")
+        key = Faker.numerify('######')
         link = link_hash gen_link(key, url)
 
         get '/meta', { :key => key }
@@ -61,7 +61,7 @@ describe PalyIO::API do
 
     describe 'GET /validate' do
       it 'makes sure a valid key is valid' do
-        custom = Faker.numerify("######")
+        custom = Faker.numerify('######')
 
         get '/validate', { :custom => custom }
 
@@ -73,13 +73,24 @@ describe PalyIO::API do
 
     describe 'GET /validate' do
       it 'makes sure an invalid key is invalid' do
-        custom = Faker.numerify("####")
+        custom = Faker.numerify('####')
 
         get '/validate', { :custom => custom }
 
         hash = JSON.parse(last_response.body)
         hash['success'].should == false
         hash['response'].should == custom
+      end
+    end
+
+    describe 'GET /qr' do
+      it 'gets a qr code' do
+        key = Faker.numerify('#####')
+
+        get '/qr', { :key => key }
+
+        hash = JSON.parse(last_response.body)
+        hash['success'].should == true
       end
     end
   end
