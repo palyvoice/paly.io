cbaa9fb1ac5e2530ce1df15e485cecaa6048a093
Max Bernstein <tekknolagi@gmail.com>
2014-05-21 13:43:14 -0700

    remove some items from TODO, changed V->U in allowed characters

--- helpers.rb
+++ helpers.rb
@@ -5,7 +5,7 @@ def host
 end
 
 def gen_rand size=6
-  charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
+  charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T U W X Y Z}
   (0...size).map{ charset.to_a[rand(charset.size)] }.join
 end
 
